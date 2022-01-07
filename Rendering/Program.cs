1c5e255f1cbb80d727393c92d3390bfe95c77677
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-01-07 21:19:54 +0100

    Add function to load shader program.

---
+++ Rendering/Program.cs
@@ -0,0 +1,34 @@
+using OpenTK.Graphics.OpenGL4;
+
+namespace PolySnake.Rendering;
+
+public static class Program
+{
+  private static int LoadShader(string code, ShaderType type)
+  {
+    var shader = GL.CreateShader(type);
+    GL.ShaderSource(shader, code);
+    GL.CompileShader(shader);
+
+    GL.GetShader(shader, ShaderParameter.CompileStatus, out var result);
+    if (result != (int) All.True)
+    {
+      var infoLog = GL.GetShaderInfoLog(shader);
+      Console.WriteLine(infoLog);
+      return -1;
+    }
+
+    return shader;
+  }
+
+  public static int Load(string vertex, string fragment)
+  {
+    var ver = LoadShader(vertex, ShaderType.VertexShader);
+    var frag = LoadShader(fragment, ShaderType.FragmentShader);
+    var id = GL.CreateProgram();
+    GL.AttachShader(id, ver);
+    GL.AttachShader(id, frag);
+    GL.LinkProgram(id);
+    return id;
+  }
+}
--- Window.cs
+++ Window.cs
@@ -3,18 +3,17 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL4;
-using PolySnake.Tools;
+using PolySnake.Rendering;
 
 namespace PolySnake;
 
 public class Window : GameWindow
 {
-  int _program;
-  int _rotate;
-  float _rot;
-
+  private Scene _scene = default!;
   Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
-      : base(gameWindowSettings, nativeWindowSettings) { }
+    : base(gameWindowSettings, nativeWindowSettings)
+  {
+  }
 
   public static Window Create()
   {
@@ -22,43 +21,17 @@ public class Window : GameWindow
     {
       Size = new Vector2i(320, 180),
       Title = "PolySnake",
-      Flags = ContextFlags.ForwardCompatible,// This is needed to run on macos
+      Flags = ContextFlags.ForwardCompatible, // This is needed to run on macos
     };
     return new Window(GameWindowSettings.Default, nativeWindowSettings);
   }
+
   protected override void OnLoad()
   {
     base.OnLoad();
-    var vertex = new []{
-      -0.5f,-0.5f,
-      -0.5f, 0.5f,
-      0.5f, 0.5f,
-
-      0.5f, 0.5f,
-      0.5f, -0.5f,
-      -0.5f, -0.5f,
-    };
-
-    var buffer = GL.GenBuffer();
-    GL.BindBuffer(BufferTarget.ArrayBuffer, buffer);
-    GL.BufferData(BufferTarget.ArrayBuffer, vertex.Length * sizeof(float), vertex, BufferUsageHint.StaticDraw);
-
-    var vertexArray = GL.GenVertexArray();
-    GL.BindVertexArray(vertexArray);
-
-    GL.Enable(EnableCap.Blend);
-    GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
-    GL.Enable(EnableCap.DepthTest);
-    GL.DepthMask(false);
-
-    _program = Gl.CreateShader(Shader.Vertex, Shader.Fragment);
-    GL.UseProgram(_program);
-    
-    _rotate = GL.GetUniformLocation(_program, "rotate");
-    var sVertexPosition = GL.GetAttribLocation(_program, "vertex_position");
-    GL.EnableVertexAttribArray(sVertexPosition);
-    GL.VertexAttribPointer(sVertexPosition, 2, VertexAttribPointerType.Float, false, 0, 0);
+    _scene = Scene.Init();
   }
+
   protected override void OnUpdateFrame(FrameEventArgs e)
   {
     // Check if the Escape button is currently being pressed.
@@ -67,24 +40,38 @@ public class Window : GameWindow
       // If it is, close the window.
       Close();
     }
+
     base.OnUpdateFrame(e);
   }
 
   protected override void OnResize(ResizeEventArgs e)
   {
     base.OnResize(e);
+    double fWidth = e.Width;
+    double fHeight = e.Height;
 
+    const double targetAspectRatio = 16.0 / 9.0;
+    double viewWidth = fWidth;
+    double viewHeight = fWidth / targetAspectRatio;
+
+    if (viewHeight > fHeight)
+    {
+      viewWidth = fHeight * targetAspectRatio;
+      viewHeight = fHeight;
+    }
+
+    double viewX = (fWidth / 2) - (viewWidth / 2);
+    double viewY = (fHeight / 2) - (viewHeight / 2);
+
+    GL.Viewport((int) viewX, (int) viewY, (int) viewWidth, (int) viewHeight);
   }
 
   protected override void OnRenderFrame(FrameEventArgs e)
   {
     base.OnRenderFrame(e);
-    var delta = (float)e.Time;
-    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-    GL.ClearColor(1f, 0.5f, 0.5f, 1f);
-    GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
-    GL.Uniform1(_rotate, _rot);
-    _rot+=0.01f;
+    //var delta = (float)e.Time;
+    _scene.Clear();
+    _scene.Show();
     SwapBuffers();
   }
 }
