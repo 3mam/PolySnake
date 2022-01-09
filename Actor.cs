bf8c7cffb47f911149665247ecdd48fac77f02b3
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-01-09 22:23:29 +0100

    Add aktor class.

---
+++ Actor.cs
@@ -0,0 +1,66 @@
+using OpenTK.Graphics.OpenGL4;
+using PolySnake.Rendering;
+using OpenTK.Mathematics;
+
+namespace PolySnake;
+
+public class Actor
+{
+  private readonly int _buffer = GL.GenBuffer();
+  private int _bufferSize;
+  private Shader _shader = default!;
+  private Vector2 _position;
+  private int _index;
+  private float _alpha = 1;
+  private float _angle;
+  private Vector2 _size = new Vector2(1f, 1f);
+
+  public static Actor Create(Shader shader) => new()
+  {
+    _shader = shader,
+  };
+
+  public void UploadData(float[] data)
+  {
+    GL.BindBuffer(BufferTarget.ArrayBuffer, _buffer);
+    GL.BufferData(BufferTarget.ArrayBuffer, data.Length * sizeof(float), data, BufferUsageHint.StaticDraw);
+    _bufferSize = data.Length / 4;
+  }
+
+  public void Position(Vector2 cord)
+  {
+    _position = cord;
+  }
+
+  public void Rotation(float degrees)
+  {
+    _angle = degrees;
+  }
+
+  public void Transparency(float alpha = 1)
+  {
+    _alpha = alpha;
+  }
+
+  public void Index(int index = 0)
+  {
+    _index = index;
+  }
+
+  public void Scale(Vector2 size)
+  {
+    _size = size;
+  }
+
+  public void Show()
+  {
+    GL.BindBuffer(BufferTarget.ArrayBuffer, _buffer);
+    _shader.Active();
+    _shader.Position(_position.X, _position.Y);
+    _shader.Rotate(_angle);
+    _shader.Alpha(_alpha);
+    _shader.Index(_index);
+    _shader.Size(_size.X, _size.Y);
+    GL.DrawArrays(PrimitiveType.Triangles, 0, _bufferSize);
+  }
+}
