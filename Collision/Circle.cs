08410667d60162738fe03fc3973063e7de0f898c
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-01-10 09:35:18 +0100

    Add circle collision.

---
+++ Collision/Circle.cs
@@ -0,0 +1,36 @@
+using OpenTK.Mathematics;
+
+namespace PolySnake.Collision;
+
+public struct Circle
+{
+  public float X;
+  public float Y;
+  public float R;
+
+  public Circle(float x, float y, float radius)
+  {
+    X = x;
+    Y = y;
+    R = radius;
+  }
+
+  public Circle(Vector2 point, float radius)
+  {
+    point.Deconstruct(out X, out Y);
+    R = radius;
+  }
+
+  public Circle(Vector3 point)
+  {
+    point.Deconstruct(out X, out Y, out R);
+  }
+
+  public bool Collide(Circle circle)
+  {
+    var x = (X + R) - (circle.X + circle.R);
+    var y = (Y + R) - (circle.Y + circle.R);
+    var distance = (float) Math.Sqrt(x * x + y * y);
+    return distance < R + circle.R;
+  }
+}
