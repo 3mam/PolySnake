9a08011c3a8316c4f29774470c4da66eb82a5e38
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-01-16 03:34:14 +0100

    Rename class and add motion method.

---
+++ SnakePosition.cs
@@ -0,0 +1,17 @@
+using OpenTK.Mathematics;
+
+namespace PolySnake;
+
+public readonly record struct SnakePosition(Vector2 Position, float Direction)
+{
+  public SnakePosition Motion(Vector2 target)
+  {
+    var direction = target - Position;
+    var angle = MathF.Atan2(direction.X, direction.Y);
+    direction.Normalize();
+    direction *= -10f;
+    return new SnakePosition(
+      target + direction,
+      -(angle * 180f / MathF.PI));
+  }
+}
