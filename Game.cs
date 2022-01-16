4fe2149e1a5e7b1fde0aad0b0b5b286873d8dfa5
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-01-16 03:34:48 +0100

    First implementation gameplay.

--- Game.cs
+++ Game.cs
@@ -7,10 +7,15 @@ public class Game
   private Actor _head = default!;
   private Actor _body = default!;
   private Actor _tail = default!;
+  private SnakePosition[] _snakePositions = default!;
+  private int _snakeLenght = 20;
   private float _scale = 0.025f;
   private Vector2 _startPosition;
   private Vector2 _position;
   private float _direction = 90f;
+  private float _viewDirection = 90f;
+  private float _speed = 200f;
+  private float _delta;
 
   public static Game Create(Scene scene)
   {
@@ -20,32 +25,71 @@ public class Game
       _body = scene.CreateActor(),
       _startPosition = new Vector2(scene.Width, scene.Height),
       _position = new Vector2(scene.Width, scene.Height),
+      _snakePositions = new SnakePosition[100],
     };
     game._head.UploadData(Snake.Head);
     game._head.Position(game._startPosition);
     game._head.Scale(game._scale);
-    
+
     game._body.UploadData(Snake.Body);
-    game._body.Scale(0.5f);
+    game._body.Scale(game._scale);
     game._body.Position(game._startPosition);
 
+    game.InitBody();
     return game;
+  }
+
+  private Vector2 MotionHead(float direction)
+  {
+    var speed = _speed * _delta;
+    var radian = direction / 180f * MathF.PI;
+    return new Vector2(
+      MathF.Cos(radian) * speed,
+      MathF.Sin(radian) * speed
+    );
   }
 
   public void Move(float delta, float direction)
   {
-    var speed = 100f * delta;
-    _direction += direction * speed;
-    var radian = _direction / 180f * MathF.PI;
-    _position.X += MathF.Cos(radian) * speed;
-    _position.Y += MathF.Sin(radian) * speed;
-    _head.Rotation(_direction - 90f);
-    _head.Position(_position);
+    _delta = delta;
+
+    _direction += direction * (_delta * _speed);
+    _position += MotionHead(_direction);
+    MoveBody();
+  }
+
+  private void InitBody()
+  {
+    for (var i = 0; i < _snakeLenght; i++)
+    {
+      _snakePositions[i] = new SnakePosition(
+        _startPosition - new Vector2(0, 15f + (10f * i)),
+        _direction - _viewDirection);
+    }
+  }
+
+  private void MoveBody()
+  {
+    _snakePositions[0] = _snakePositions[0].Motion(_position);
+    for (int i = 1; i < _snakeLenght; i++)
+      _snakePositions[i] = _snakePositions[i].Motion(_snakePositions[i - 1].Position);
+  }
+
+  private void DrawBody()
+  {
+    for (var i = 0; i < _snakeLenght; i++)
+    {
+      _body.Rotation(_snakePositions[i].Direction);
+      _body.Position(_snakePositions[i].Position);
+      _body.Draw();
+    }
   }
 
   public void Draw()
   {
-    _head.Show();
-    _body.Show();
+    _head.Rotation(_direction - _viewDirection);
+    _head.Position(_position);
+    _head.Draw();
+    DrawBody();
   }
 }
