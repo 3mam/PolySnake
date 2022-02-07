0942061e5ce383099aeb4d4ecc1cb4eb49ae6125
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-02-07 15:28:58 +0100

    Change struct to class and add direction limiter.

--- SnakePosition.cs
+++ SnakePosition.cs
@@ -1,9 +1,29 @@
+using System.Diagnostics;
 using OpenTK.Mathematics;
 
 namespace PolySnake;
 
-public readonly record struct SnakePosition(Vector2 Position, float Direction)
+public class SnakePosition
 {
+  private readonly float _direction;
+  public Vector2 Position { get;}
+  public float Direction
+  {
+    get => _direction switch
+    {
+      < 0 => 360f + _direction,
+      > 360 => _direction - 360f,
+      _ => _direction
+    };
+    private init => _direction = value;
+  }
+
+  public SnakePosition(Vector2 position, float direction)
+  {
+    Position = position;
+    Direction = direction;
+  }
+
   public SnakePosition Motion(Vector2 target)
   {
     var direction = target - Position;
