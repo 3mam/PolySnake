b67510fe854ce12a2ab62f99490cc5d27502e840
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-03-09 15:36:18 +0100

    Add collision test.

---
+++ Test/CollisionTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game.Collision;
+
+namespace Test;
+
+[TestClass]
+public class CollisionTest
+{
+  [TestMethod]
+  public void CircleToCircle()
+  {
+    var a = new CollideCircle(10, 10, 10);
+    var b = new CollideCircle(15, 15, 10);
+    Assert.IsTrue(Collide.CircleToCirecle(a, b));
+  }
+
+  [TestMethod]
+  public void LineToLine()
+  {
+    var a = new CollideLine(10, 10, 20, 10);
+    var b = new CollideLine(15, 5, 15, 15);
+    Assert.IsTrue(Collide.LineToLine(a, b));
+    Assert.IsTrue(a==b);
+  }
+
+  [TestMethod]
+  public void LineToCircle()
+  {
+    var a = new CollideLine(10, 10, 20, 10);
+    var b = new CollideCircle(15, 5, 5);
+    Assert.IsTrue(Collide.LineToCircle(a, b));   
+    Assert.IsTrue(a==b);
+  }
+}
