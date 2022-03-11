5f7eccb01640abb002aa6490f50784e1250044ce
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-03-11 10:31:29 +0100

    Added simple level.

---
+++ Game/Level.cs
@@ -0,0 +1,14 @@
+namespace Game;
+
+public static class Level
+{
+  public static float[] Zero = new[]
+  {
+    -0.18f, -0.32f,
+    -0.18f, 0.32f,
+    0.18f, 0.32f,
+    0.18f, 0.32f,
+    0.18f, -0.32f,
+    -0.18f, -0.32f,
+  };
+}
