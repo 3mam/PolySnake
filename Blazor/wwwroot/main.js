519f4c8836c879953c80a151a23eed72c0756722
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-04-01 21:59:10 +0200

    Add key input for Blazor. Q is turn left, R is turn right.

--- Blazor/wwwroot/main.js
+++ Blazor/wwwroot/main.js
@@ -7,13 +7,21 @@ function init() {
 var scene = () =>
     Scene.create(800,600,1)
 
+var key = ""
+document.addEventListener('keypress', (event) => {
+    key = event.key
+})
+
+document.addEventListener('keyup', (event) => {
+    key = ""
+})
 
 function loop() {
     let last = 0
     const insideLoop = timestamp => {
         const delta = Math.min(1, (timestamp - last) / 1000)
         last = timestamp
-        DotNet.invokeMethodAsync('Blazor', 'Run', delta)
+        DotNet.invokeMethodAsync('Blazor', 'Run', delta, key)
         window.requestAnimationFrame(insideLoop)
     }
     insideLoop(0)
