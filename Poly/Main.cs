f32559a0ec5acb9505dbff8592f4072f02838ad3
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-04-04 11:13:10 +0200

    Hide the console on Windows.

--- Poly/Main.cs
+++ Poly/Main.cs
@@ -1,6 +1,20 @@
-﻿using Poly;
+﻿using System.Runtime.InteropServices;
+using Poly;
 
 Console.WriteLine("Hello, World!");
+
+//Hide the console on Windows.
+if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+{
+  [DllImport("kernel32.dll")]
+  static extern IntPtr GetConsoleWindow();
+
+  [DllImport("user32.dll")]
+  static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+  var handle = GetConsoleWindow();
+  ShowWindow(handle, 0);
+}
 
 using var window = Window.Create();
 window.Run();
