5dbb13a97368ab120d32ae5462354386846338e3
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-04-12 22:06:44 +0200

    Adding class for point calculation.

---
+++ Game/Math/Point.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Game.Math;
+
+public class Point
+{
+  private readonly Vector2 _point;
+  private Vector2 _pivot;
+  private Vector2 _position;
+  private float _angle;
+  private int _scale = 1;
+
+  public Vector2 Value
+  {
+    get
+    {
+      var s = MathF.Sin(_angle);
+      var c = MathF.Cos(_angle);
+      var x = _point.X - _pivot.X;
+      var y = _point.Y - _pivot.Y;
+      var xNew = x * c - y * s;
+      var yNew = x * s + y * c;
+      return new Vector2(
+        (xNew + _pivot.X) * _scale + _position.X,
+        (yNew + _pivot.Y) * _scale + _position.Y
+      );
+    }
+  }
+
+  public Point(Vector2 point)
+    => _point = point;
+
+  public Point Pivot(Vector2 point)
+  {
+    _pivot = point;
+    return this;
+  }
+
+  public Point Rotate(float angle)
+  {
+    _angle = angle;
+    return this;
+  }
+
+  public Point Scale(int size)
+  {
+    _scale = size;
+    return this;
+  }
+
+  public Point Position(Vector2 point)
+  {
+    _position = point;
+    return this;
+  }
+}
